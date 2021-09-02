68244e1896e7280a96fffdfbf72d7e531d515a14
Gustavo Brasil <gubrasil@gmail.com>
2021-09-01 22:42:36 -0300

    atualiza main

--- src/Components/Registros/addEntrada.jsx
+++ src/Components/Registros/addEntrada.jsx
@@ -1,63 +1,88 @@
 import React, { useState } from 'react';
-import {Redirect}  from 'react-router-dom';
-
 import firebase from '../../Config/firebase';
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
+import { Redirect } from 'react-router-dom';
+import { registerLocale } from "react-datepicker";
+import pt from 'date-fns/locale/pt';
+import './addEntrada.css';
 
+
+registerLocale('pt', pt);
 
 function AddEntrada() {
 
-    const nomeEmpresa = "Mentoria Conquiste Sua Vaga"
-    const nomeFuncionario = "Aluno da Mentoria Conquiste Sua Vaga"
-    const db = firebase.firestore();
-    const inicioIntervalohorahoraEntrada = "";
-    const fimIntervalo = "";
-    const horaSaida = "";
-    const horasTrabalhadas = "";      
-    const data = new Date().toLocaleDateString('pt-BR', {
-      day: 'numeric',
-      month: '2-digit',
-      year: 'numeric',
-    })
+  const nomeEmpresa = "Mentoria Conquiste Sua Vaga"
+  const nomeFuncionario = "Aluno da Mentoria Conquiste Sua Vaga"
+  const db = firebase.firestore();
+  const inicioIntervalohorahoraEntrada = "";
+  const fimIntervalo = "";
+  const horaSaida = "";
+  const horasTrabalhadas = "";
+  const [startDate, setStartDate] = useState(new Date());
+  const [sucesso, setSucesso] = useState('N');
+  // const entrada = "N";
 
-    const HoraEntrada = new Date().toLocaleDateString('pt-BR', {
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    })
 
-    const [sucesso, setSucesso] = useState('N');
-  
-    function registraEntrada() {
-            db.collection('pontoEletronico').add({
-                Empresa: nomeEmpresa,
-                Funcionario: nomeFuncionario,
-                data: data,
-                Hora_entrada: HoraEntrada,
-                Inicio_intervalo: inicioIntervalohorahoraEntrada,
-                Fim_intervalo: fimIntervalo,
-                Hora_saida: horaSaida,
-                Horas_trabalhadas: horasTrabalhadas
-            }).then(async() => {
-                 setSucesso('S');
-                // await CriaLogin();
-            }).catch((erro) => {
-                // setMensagem(erro);
-                 setSucesso('N');
-            })
-        }  
-  
-    function registraSaida() {
-      
-    }
-    
-    return (<section>
+  // function selectedDate(date) {
+  //   console.log(date.toLocaleDateString());
+  //   console.log(date.toLocaleTimeString());
+  // new Date().toLocaleTimeString()
+  //   setStartDate(date);
+  //   const parsedDate  = parseISO(date);
+  //   const past = isAfter(parsedDate, new Date()); // true
+  //   console.log(past);
+  // }
+
+
+  function verificaEntrada() {
+    firebase.firestore().collection('pontoEletronico').get().then(async function (resultado) {
+      await resultado.docs.forEach(function (doc) {
+        console.log(doc.data().Funcionario);
+        console.log(doc.data().Data);
+      })
+
+    })
+  }
+
+
+  function registraEntrada() {
+    verificaEntrada();
+    db.collection('pontoEletronico').add({
+      Empresa: nomeEmpresa,
+      Funcionario: nomeFuncionario,
+      Data: startDate.toLocaleDateString(),
+      Hora_entrada: startDate.toLocaleTimeString(),
+      Inicio_intervalo: inicioIntervalohorahoraEntrada,
+      Fim_intervalo: fimIntervalo,
+      Hora_saida: horaSaida,
+      Horas_trabalhadas: horasTrabalhadas
+    }).then(async () => {
+      setSucesso('S');
+      console.log(setStartDate);
+      // await CriaLogin();
+    }).catch((erro) => {
+      // setMensagem(erro);
+      setSucesso('N');
+    })
+  }
+
+  return (<section>
+    <div className="input-container">
+      <div className="row">
+        <div>
+          <label>Data</label>
+          <DatePicker locale="pt" dateFormat="dd/MM/yyyy" selected={startDate} onChange={(date) => setStartDate(date)} />
+        </div>
+      </div>
       <div className="text-center">
-        <button onClick={registraEntrada} className="btn btn-primary" type="button">Entrada</button>
+        <button id="button-add" onClick={registraEntrada} className="btn btn-primary" type="button">Entrada</button>
         {sucesso === 'S' ? <Redirect to='/' /> : null}
         {/* <button onClick={registraSaida} className="btn btn-primary" type="button">Saida</button> */}
       </div>
-    </section>
-    )
-  }
-  
-  export default AddEntrada;
+    </div>
+  </section>
+  )
+}
+
+export default AddEntrada;
--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -9,79 +9,69 @@ import 'firebase/firestore';
 
 // import SweetAlert from 'react-bootstrap-sweetalert';
 
-function Main(){
+function Main() {
 
   const [ponto, setPonto] = useState([]);
   const [busca, setBusca] = useState('');
   const [texto, setTexto] = useState('');
-
-//   const [confirmacao, setConfirmacao] = useState(false);
-//   const [confirmacaoId, setConfirmacaoId] = useState('');
+  const [Funcionario, setFuncionario] = useState('');
 
 
-  useEffect(function(){
+  //   const [confirmacao, setConfirmacao] = useState(false);
+  //   const [confirmacaoId, setConfirmacaoId] = useState('');
+
+
+  useEffect(function () {
     let listaPonto = [];
-    
-      firebase.firestore().collection('pontoEletronico').get().then(async function(resultado){
-          await resultado.docs.forEach(function(doc){
-              if (doc.data().Funcionario.indexOf(busca) >= 0) {
-                listaPonto.push({
-                        id: doc.id,
-                        Empresa: doc.data().Empresa,
-                        Funcionario: doc.data().Funcionario,
-                        Data: doc.data().Data,
-                        Hora_entrada: doc.data().Hora_entrada, 
-                        Inicio_intervalo: doc.data().Inicio_intervalo, 
-                        Fim_intervalo: doc.data().Fim_intervalo, 
-                        Hora_saida: doc.data().Hora_saida
-                    });
-                }
-          })
 
-          setPonto(listaPonto);
+    firebase.firestore().collection('pontoEletronico').get().then(async function (resultado) {
+      await resultado.docs.forEach(function (doc) {
+        if (doc.data().Funcionario.indexOf(busca) >= 0) {
+          listaPonto.push({
+            id: doc.id,
+            Empresa: doc.data().Empresa,
+            Funcionario: doc.data().Funcionario,
+            Data: doc.data().Data,
+            Hora_entrada: doc.data().Hora_entrada,
+            Inicio_intervalo: doc.data().Inicio_intervalo,
+            Fim_intervalo: doc.data().Fim_intervalo,
+            Hora_saida: doc.data().Hora_saida
+          });
+        }
       })
-    }, [busca]);
 
-    return <div>
-      <div className="container-fluid titulo">
-        <h1>Ponto de Funcionário</h1>
+      setPonto(listaPonto);
+    })
+  }, [busca]);
 
-        <div className="row">
-          <div className="col-4">
-          <AddEntrada />
-
-            {/* <Link to='/app/novocliente' className="btn btn-primary" type="button"><i className="fas fa-plus"></i> Cliente</Link> */}
+  return <div>
+    <div className="container-fluid titulo">
+      <h1>Ponto de Funcionário</h1>
+      <div className="row">
+        
+        <div className="col-8">
+         
+        </div>
+        <div className="col-4">
+          <label htmlFor="exampleInputEmail1" className="form-label">Digite seu nome</label>
+          <input onChange={(e) => setFuncionario(e.target.value)} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-4" >
+          <div className="input-group mb-3">
+            <input onChange={(e) => setTexto(e.target.value)} type="text" className="form-control" placeholder="Pesquisar por nome" aria-describedby="button-addon2" />
+            <button onClick={(e) => setBusca(texto)} className="btn btn-primary" type="button" id="button-addon2"><i className="fas fa-search"></i> Pesquisar</button>
           </div>
+        </div>
+        <AddEntrada />
 
-          <div className="col-4">
-            <div className="input-group mb-3">
-              <input onChange={(e) => setTexto(e.target.value)} type="text" className="form-control" placeholder="Pesquisar por nome" aria-describedby="button-addon2" />
-              <button onClick={(e) => setBusca(texto)} className="btn btn-primary" type="button" id="button-addon2"><i className="fas fa-search"></i> Pesquisar</button>
-            </div>
-          </div>
-        </div>        
- 
-        <ListarPonto arrayPonto={ponto} />
 
-        {/* {
-          confirmacao ? <SweetAlert
-                        warning
-                        showCancel
-                        showCloseButton
-                        confirmBtnText="Sim"
-                        confirmBtnBsStyle="danger"
-                        cancelBtnText="Não"
-                        cancelBtnBsStyle="light"
-                        title="Exclusão"
-                        onConfirm={() => deleteUser(confirmacaoId)}
-                        onCancel={() => setConfirmacao(false)}          
-                        reverseButtons={true}
-                      >
-                        Deseja excluir o cliente selecionado?
-                      </SweetAlert> : null } */}
+      </div>
+      <ListarPonto arrayPonto={ponto} />
 
-      </div>      
     </div>
-  }
+  </div>
+}
 
 export default Main;
